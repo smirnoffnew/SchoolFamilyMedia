b0e60418477e9c724e8ced3a64abcd88558fd11c
smirnoffnew <smirnoffnew@gmail.com>
2020-02-20 14:32:20 +0200

    Updates

---
+++ precache-manifest.992744f3697cb0e17c94e4f14bb2fe75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5488e73857289258f8afeddb849af26",
+    "url": "/smirnoffnew/SchoolFamilyMedia/index.html"
+  },
+  {
+    "revision": "cfeb1bf6ec9f7272af23",
+    "url": "/smirnoffnew/SchoolFamilyMedia/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "faf9d3c4218c92831365",
+    "url": "/smirnoffnew/SchoolFamilyMedia/static/js/2.7c0128a0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/smirnoffnew/SchoolFamilyMedia/static/js/2.7c0128a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfeb1bf6ec9f7272af23",
+    "url": "/smirnoffnew/SchoolFamilyMedia/static/js/main.f14ce61e.chunk.js"
+  },
+  {
+    "revision": "ac22cf2570fd01cd9644",
+    "url": "/smirnoffnew/SchoolFamilyMedia/static/js/runtime-main.d840564f.js"
+  }
+]);
